871653e07cba31dde95ab49e55e276c0c54c9864
Anika Knyshenko <anika.codeclub.dnipro@gmail.com>
2025-05-05 07:56:10 +0300

    new proj

---
+++ kids/CSS_JS/23_treasure/Denis/script.js
@@ -0,0 +1,19 @@
+#map {
+  width: 400px;
+  height: 400px;
+  background-image: url("image.png");
+  background-size: cover;
+  margin: 20px auto;
+  position: relative;
+  border: 3px solid #fff;
+  border-radius: 10px;
+  cursor: crosshair;
+}
+body {
+  text-align: center;
+  background-color: #fff638;
+}
+#message {
+  margin-top: 10px;
+  font-size: 18px;
+}
