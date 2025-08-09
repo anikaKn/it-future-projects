79da2fbecdae0fcaf385f302deb3c1d92c5bd698
Anika Knyshenko <anika.codeclub.dnipro@gmail.com>
2025-08-09 17:23:36 +0300

    New projects

---
+++ CSS_JS/Vidpovidi-na-pitannia/script.js
@@ -0,0 +1,12 @@
+var newbtn = document.getElementById("newbtn");
+newbtn.addEventListener('click', function() {
+  document.getElementById('new').innerHTML = `
+  <button id="newbtn">Ха Ха ха</button>
+  <br>
+  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZrsiWk3DpZCYQYhFK9vBvdy9x1IqPrk-LDQ&usqp=CAU" alt="Image" width="200" height="200"
+  <br>
+  <a href='https://www.youtube.com/watch?v=pBnEhKEVtoY&ab_channel=%D0%92%D0%B8%D1%88%D0%B8%D0%B2%D0%B0%D0%BD%D1%87%D0%B8%D0%BA' target='_black'>Посиланнячко на ютуб</a>
+      `;
+})
+
+
